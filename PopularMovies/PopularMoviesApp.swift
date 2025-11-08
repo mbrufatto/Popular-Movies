dd9902d96e5de98981c9067cf710917f61c5adee
Márcio Habigzang Brufatto <mhbrufatto@gmail.com>
2025-11-07 21:20:32 -0300

    Initial Commit

---
+++ PopularMovies/PopularMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopularMoviesApp.swift
+//  PopularMovies
+//
+//  Created by Marcio Habigzang Brufatto on 07/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PopularMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
